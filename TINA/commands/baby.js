f54d2f215d581b2326dd56c51cd94e0e1db3585c
sanjidakhan8 <sanjidakhan8234@gmail.com>
2025-03-17 00:46:20 +0600

    Update baby.js

--- TINA/commands/baby.js
+++ TINA/commands/baby.js
@@ -1,9 +1,7 @@
 const axios = require('axios');
 
 const baseApiUrl = async () => {
-    const base = await axios.get(`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`);
-    console.log(base.data.api);
-    return base.data.api;
+     return "https://www.noobs-api.rf.gd/dipto";
 };
 
 module.exports.config = {
